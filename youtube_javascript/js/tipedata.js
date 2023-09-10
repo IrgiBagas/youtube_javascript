e2e7b105a5e54a57b96e88d683d55a41c53825af
IrgiBagas <140321137+IrgiBagas@users.noreply.github.com>
2023-09-10 15:23:56 +0700

    Add files via upload

---
+++ youtube_javascript/js/tipedata.js
@@ -0,0 +1,27 @@
+let angka = 7;
+
+let kata = "menamatkan belajar javascript";
+
+let pecahan = 2.2;
+
+console.log(angka);
+
+console.log(typeof angka);
+
+console.log(1 / 0);
+
+console.log("javasript / 2 + 3");
+
+let isi = "sedang";
+console.log(isi+kata);
+console.log(` 'irgi' ${isi} ${kata} ${angka}`)
+console.log("irgi"+isi+kata+isi);
+
+let kondisi = true;
+console.log(kondisi);
+
+let age = null;
+console.log(age);
+
+let kosong;
+console.log(kosong);
---
+++ youtube_javascript/js/variabel.js
@@ -0,0 +1,15 @@
+let angka;
+let kata = "javascript";
+
+const data = 100;
+angka = 45;
+
+console.log(angka);
+console.log(kata);
+console.log(data);
+
+angka = 57;
+console.log(angka);
+
+let isi = 7;
+console.log(typeof isi);
